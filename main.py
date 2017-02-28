55b379cdf480da1e9569b3d87f19fc69b605751e
Sharidan Barboza <barboza@cs.ualberta.ca>
2017-02-28 13:44:05 -0700

    Add exit option

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
+import sys
+
 from connect import get_connection
+from utils import display_selections
+from validate import get_selection
+
+def get_user(conn):
+	choices = ["Login", "Sign up", "Exit"]
+	display_selections(choices)
+	choice = get_selection(len(choices))
+
+	username = None
+
+	if choice == 3:
+		sys.exit()
+
+	return ""
 
 # ----------------------------------- MAIN --------------------------------------
 
 def main():
     # Connect to database
     conn = get_connection("sql_login.txt")
-    curs = conn.cursor()
+
+    # Opening screen
+    username = get_user(conn)
 
 if __name__ == "__main__":
 	main()
