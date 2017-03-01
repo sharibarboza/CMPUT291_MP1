329e8a4e173721cf7d208e704d1c5a5f03bd8c74
Sharidan Barboza <barboza@cs.ualberta.ca>
2017-02-28 20:46:53 -0700

    Add length optional arg to validate_str

--- validate.py
+++ validate.py
@@ -1,14 +1,18 @@
 # Input validation methods
 
-def validate_str(prompt):
+def validate_str(prompt, length=None):
     """
     Used for when user needs to input words
     Ensures that user input is non-empty
     """
-    usr_input = ""
-    while len(usr_input) == 0:
+    valid = False
+    while not valid:
     	usr_input = input(prompt)
-    return usr_input
+    	if length and len(usr_input) > length:
+    		print("Input must be %d characters or less." % (length))
+    		valid = False
+    	else:
+    		return usr_input
 
 def validate_num(prompt, size=None):
     """
@@ -22,10 +26,10 @@ def validate_num(prompt, size=None):
             if size and choice not in range(1, size + 1):
             	raise ValueError
         except ValueError:
-        	if size:
-        		print("Selection must be a number from 1 to %d." % (size))
-        	else:
-        		print("Please enter a number.")
-        	valid = False
+            if size:
+            	print("Selection must be a number from 1 to %d." % (size))
+            else:
+            	print("Please enter a number.")
+            valid = False
         else:
             return choice
