c58e5e451bff4e7aaeb91193d72c75f269f52698
Sharidan Barboza <barboza@cs.ualberta.ca>
2017-02-28 20:16:00 -0700

    Change function names

--- validate.py
+++ validate.py
@@ -2,7 +2,7 @@ from constants import SELECT
 
 # Input validation methods
 
-def get_input(prompt):
+def validate_str(prompt):
     """
     Used for when user needs to input words
     Ensures that user input is non-empty
@@ -12,7 +12,7 @@ def get_input(prompt):
     	usr_input = input(prompt)
     return usr_input
 
-def get_selection(size):
+def validate_num(size):
     """
     Used for when user needs to input a single number 
     Used mainly for menu selections
