49ea5cc81078131ee7156dc6e40d07501f3102f4
Sharidan Barboza <barboza@cs.ualberta.ca>
2017-03-02 17:25:35 -0700

    Indicate user retweets

--- tweet.py
+++ tweet.py
@@ -9,7 +9,7 @@ class TweetSearch:
         self.user = user
 
         if user:
-        	self.get_user_tweets()
+            self.get_user_tweets()
 
     def get_user_tweets(self):
         curs = self.conn.cursor()
@@ -20,24 +20,27 @@ class TweetSearch:
         rows = curs.fetchmany(numRows = 5)
 
         for i, row in enumerate(rows, 1):
-        	print(BORDER)
-        	t_writer = row[1]
-        	t_date = convert_date(row[2])
-        	t_text = row[3]
-        	t_replyto = row[4]
-        	t_rt = row[5]
-        	writer_name = get_name(curs, t_writer)
+            print(BORDER)
+            t_writer = row[1]
+            t_date = convert_date(row[2])
+            t_text = row[3]
+            t_replyto = row[4]
+            t_rt = row[5]
+            writer_name = get_name(curs, t_writer)
 
-        	print("Tweet %d" % (i))
-        	print("%s @%d - %s" % (writer_name, t_writer, t_date))
-        	print(t_text)
+            print("Tweet %d" % (i))
+
+            # Indicate retweet
+            if self.is_retweet(t_writer, t_rt):
+                rt_name = get_name(curs, t_rt)
+                print("%s Retweeted" % (rt_name))
+
+            print("%s @%d - %s" % (writer_name, t_writer, t_date))
+            print(t_text)
 
         print(BORDER)
 
-
-
-
-
-
+    def is_retweet(self, writer, usr):
+        return writer != usr
 
 
