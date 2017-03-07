e2ffaebb20674b4d95b616b5c80065bc8062d018
sharibarboza <barbozashari@gmail.com>
2017-03-06 22:19:54 -0700

    Add UserSearch

--- user.py
+++ user.py
@@ -1,1 +1,11 @@
-# Functionality #2
+from constants import SELECT, TODAY, BORDER, BORDER_LEN
+from utils import *
+from queries import * 
+
+
+class UserSearch:
+
+	def __init__(self, session, user, keywords=''):
+		self.session = session
+		self.user = user
+		self.keywords = convert_keywords(keywords)
